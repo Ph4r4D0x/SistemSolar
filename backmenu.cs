9f6f0cffdd6267a63af7230d9e0b87576d473e7b
ACER <ACER@ACER>
2024-03-21 14:21:57 +0200

    Aggiungere i file di progetto.

---
+++ backmenu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemSolar
+{
+    public class Menu
+    {
+        private List<SistemSolar> sistemeSolare = new List<SistemSolar>();
+        private SistemSolar sistemSolarCurent = null;
+
+        public void AfisareMeniu()
+        {
+            Console.WriteLine("A. Citire sistem solar");
+            Console.WriteLine("B. Afisare informatii despre ultimul sistem solar introdus");
+            Console.WriteLine("C. Afisare toate sistemele solare");
+            Console.WriteLine("D. Salvare sisteme solare");
+            Console.WriteLine("E. Inchidere program");
+        }
+
+        public void CitireSistemSolar()
+        {
+            Console.WriteLine("Introduceti numele sistemului:");
+            string numeSistem = Console.ReadLine();
+
+            Console.WriteLine("Introduceti numele stelei:");
+            string numeStea = Console.ReadLine();
+
+            Console.WriteLine("Introduceti numarul de planete:");
+            int nrPlanete;
+
+            while (!int.TryParse(Console.ReadLine(), out nrPlanete))
+            {
+                Console.WriteLine("Input esuat, va rog sa dati un numar intreg:");
+            }
+
+            sistemSolarCurent = new SistemSolar(numeSistem, numeStea, nrPlanete);
+            sistemeSolare.Add(sistemSolarCurent);
+        }
+
+        public void AfisareUltimSistemSolar()
+        {
+            if (sistemSolarCurent != null)
+            {
+                Console.WriteLine(sistemSolarCurent.Info());
+            }
+            else
+            {
+                Console.WriteLine("Nu exista sisteme solare introduse.");
+            }
+        }
+
+        public void AfisareToateSistemeleSolare()
+        {
+            if (sistemeSolare.Count > 0)
+            {
+                foreach (var sistem in sistemeSolare)
+                {
+                    Console.WriteLine(sistem.Info());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nu exista sisteme solare de afisat.");
+            }
+        }
+
+        public void SalvareSistemeSolare()
+        {
+
+            Console.WriteLine("Salvat sistemele solare. (Implementare necesara)");
+        }
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SistemSolar
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Menu menu = new Menu();
+            string optiune;
+
+            do
+            {
+                menu.AfisareMeniu();
+                Console.WriteLine("Alegeti o optiune:");
+                optiune = Console.ReadLine();
+
+                switch (optiune.ToUpper())
+                {
+                    case "A":
+                        menu.CitireSistemSolar();
+                        break;
+                    case "B":
+                        menu.AfisareUltimSistemSolar();
+                        break;
+                    case "C":
+                        menu.AfisareToateSistemeleSolare();
+                        break;
+                    case "D":
+                        menu.SalvareSistemeSolare();
+                        break;
+                    case "E":
+                        Console.WriteLine("Inchidere program...");
+                        break;
+                    default:
+                        Console.WriteLine("Optiune inexistenta!");
+                        break;
+                }
+            } while (optiune.ToUpper() != "E");
+
+            Console.ReadKey();
+        }
+    }
+}
