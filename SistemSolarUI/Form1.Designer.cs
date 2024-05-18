fac4db35d57f45424b12d6ad94e471c00f7f37e4
Cristian Zagan <zaganc07@gmail.com>
2024-05-18 21:00:20 +0300

    Am folosit si implementat ToolBox si implementat functia de a modifica datele prin selectia prin ID

--- SistemSolarUI/Form1.Designer.cs
+++ SistemSolarUI/Form1.Designer.cs
@@ -2,45 +2,212 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBoxSystems;
+        private System.Windows.Forms.ComboBox comboBoxSystems;
+        private System.Windows.Forms.TextBox textBoxNameSystem;
+        private System.Windows.Forms.TextBox textBoxStarName;
+        private System.Windows.Forms.TextBox textBoxPlanetNumber;
+        private System.Windows.Forms.TextBox textBoxSearchID;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label labelNameSystem;
+        private System.Windows.Forms.Label labelStarName;
+        private System.Windows.Forms.Label labelPlanetNumber;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            listBoxSystems = new ListBox();
+            comboBoxSystems = new ComboBox();
+            textBoxNameSystem = new TextBox();
+            textBoxStarName = new TextBox();
+            textBoxPlanetNumber = new TextBox();
+            textBoxSearchID = new TextBox();
+            buttonAdd = new Button();
+            buttonSearch = new Button();
+            buttonModify = new Button();
+            checkBox1 = new CheckBox();
+            radioButton1 = new RadioButton();
+            labelError = new Label();
+            labelNameSystem = new Label();
+            labelStarName = new Label();
+            labelPlanetNumber = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 150);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += new DataGridViewCellEventHandler(dataGridView1_CellContentClick);
+            // 
+            // listBoxSystems
+            // 
+            listBoxSystems.FormattingEnabled = true;
+            listBoxSystems.ItemHeight = 15;
+            listBoxSystems.Location = new Point(12, 168);
+            listBoxSystems.Name = "listBoxSystems";
+            listBoxSystems.Size = new Size(776, 94);
+            listBoxSystems.TabIndex = 1;
+            // 
+            // comboBoxSystems
+            // 
+            comboBoxSystems.FormattingEnabled = true;
+            comboBoxSystems.Location = new Point(12, 269);
+            comboBoxSystems.Name = "comboBoxSystems";
+            comboBoxSystems.Size = new Size(776, 23);
+            comboBoxSystems.TabIndex = 2;
+            // 
+            // textBoxNameSystem
+            // 
+            textBoxNameSystem.Location = new Point(12, 337);
+            textBoxNameSystem.Name = "textBoxNameSystem";
+            textBoxNameSystem.Size = new Size(200, 23);
+            textBoxNameSystem.TabIndex = 3;
+            // 
+            // textBoxStarName
+            // 
+            textBoxStarName.Location = new Point(218, 337);
+            textBoxStarName.Name = "textBoxStarName";
+            textBoxStarName.Size = new Size(200, 23);
+            textBoxStarName.TabIndex = 4;
+            // 
+            // textBoxPlanetNumber
+            // 
+            textBoxPlanetNumber.Location = new Point(424, 337);
+            textBoxPlanetNumber.Name = "textBoxPlanetNumber";
+            textBoxPlanetNumber.Size = new Size(200, 23);
+            textBoxPlanetNumber.TabIndex = 5;
+            // 
+            // textBoxSearchID
+            // 
+            textBoxSearchID.Location = new Point(12, 373);
+            textBoxSearchID.Name = "textBoxSearchID";
+            textBoxSearchID.Size = new Size(200, 23);
+            textBoxSearchID.TabIndex = 6;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(630, 334);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(158, 23);
+            buttonAdd.TabIndex = 7;
+            buttonAdd.Text = "Adauga Sistem Solar";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
+            // 
+            // buttonSearch
+            // 
+            buttonSearch.Location = new Point(218, 373);
+            buttonSearch.Name = "buttonSearch";
+            buttonSearch.Size = new Size(158, 23);
+            buttonSearch.TabIndex = 8;
+            buttonSearch.Text = "Cauta dupa ID";
+            buttonSearch.UseVisualStyleBackColor = true;
+            buttonSearch.Click += buttonSearch_Click;
+            // 
+            // buttonModify
+            // 
+            buttonModify.Location = new Point(382, 373);
+            buttonModify.Name = "buttonModify";
+            buttonModify.Size = new Size(158, 23);
+            buttonModify.TabIndex = 15;
+            buttonModify.Text = "Modify";
+            buttonModify.UseVisualStyleBackColor = true;
+            buttonModify.Click += buttonModify_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 409);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(84, 409);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 10;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // labelError
+            // 
+            labelError.AutoSize = true;
+            labelError.ForeColor = Color.Red;
+            labelError.Location = new Point(12, 438);
+            labelError.Name = "labelError";
+            labelError.Size = new Size(197, 15);
+            labelError.TabIndex = 11;
+            labelError.Text = "Toate campurile trebuie completate.";
+            // 
+            // labelNameSystem
+            // 
+            labelNameSystem.AutoSize = true;
+            labelNameSystem.Location = new Point(12, 321);
+            labelNameSystem.Name = "labelNameSystem";
+            labelNameSystem.Size = new Size(78, 15);
+            labelNameSystem.TabIndex = 12;
+            labelNameSystem.Text = "Nume Sistem";
+            // 
+            // labelStarName
+            // 
+            labelStarName.AutoSize = true;
+            labelStarName.Location = new Point(215, 321);
+            labelStarName.Name = "labelStarName";
+            labelStarName.Size = new Size(36, 15);
+            labelStarName.TabIndex = 13;
+            labelStarName.Text = "Soare";
+            // 
+            // labelPlanetNumber
+            // 
+            labelPlanetNumber.AutoSize = true;
+            labelPlanetNumber.Location = new Point(421, 321);
+            labelPlanetNumber.Name = "labelPlanetNumber";
+            labelPlanetNumber.Size = new Size(65, 15);
+            labelPlanetNumber.TabIndex = 14;
+            labelPlanetNumber.Text = "Nr. Planete";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(buttonModify);
+            Controls.Add(labelPlanetNumber);
+            Controls.Add(labelStarName);
+            Controls.Add(labelNameSystem);
+            Controls.Add(labelError);
+            Controls.Add(radioButton1);
+            Controls.Add(checkBox1);
+            Controls.Add(buttonSearch);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxSearchID);
+            Controls.Add(textBoxPlanetNumber);
+            Controls.Add(textBoxStarName);
+            Controls.Add(textBoxNameSystem);
+            Controls.Add(comboBoxSystems);
+            Controls.Add(listBoxSystems);
+            Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
-
-        #endregion
     }
 }
--- SistemSolarUI/Form1.cs
+++ SistemSolarUI/Form1.cs
@@ -1,144 +1,28 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace SistemSolarUI
 {
     public partial class Form1 : Form
     {
-        private ListView lvSistemeSolare;
-        private TextBox txtNumeSistem;
-        private TextBox txtNumeStea;
-        private TextBox txtNrPlanete;
-        private Button btnAdauga;
         private const string CALE_FISIER = @"C:\Users\ACER\Desktop\Uni\PIU\SSIV\SistemSolar\bin\Debug\DateSS.txt";
-        private ListBox listBoxSisteme;
-        private ComboBox comboBoxSisteme;
-        private TextBox txtIdCautat;
-        private Button btnCauta;
 
         public Form1()
         {
             InitializeComponent();
-            InitializeazaComponenteUI();
-            InitializeazaListView();
-            this.AutoScroll = true;
+            InitializeDataGridView();
             this.Load += Form1_Load;
         }
 
-        private Label lblEroare;
-
-        private void InitializeazaComponenteUI()
+        private void InitializeDataGridView()
         {
-            txtNumeSistem = new TextBox { Location = new Point(10, this.Height - 100), Width = 200 };
-            txtNumeStea = new TextBox { Location = new Point(220, this.Height - 100), Width = 200 };
-            txtNrPlanete = new TextBox { Location = new Point(430, this.Height - 100), Width = 200 };
-
-            btnAdauga = new Button { Text = "Adauga Sistem Solar", Location = new Point(640, this.Height - 100), Width = 200 };
-            btnAdauga.Click += btnAdauga_Click;
-
-            
-            lblEroare = new Label
-            {
-                Location = new Point(10, this.Height - 70),
-                Width = 830,
-                ForeColor = Color.Red,
-                Text = ""
-            };
-
-            RadioButton radioButton1 = new RadioButton
-            {
-                Text = "Optiune 1",
-                Location = new Point(640, this.Height - 150),
-                AutoSize = true
-            };
-            radioButton1.CheckedChanged += RadioButton1_CheckedChanged;
-
-            
-            CheckBox checkBox1 = new CheckBox
-            {
-                Text = "Bifat",
-                Location = new Point(640, this.Height - 180),
-                AutoSize = true
-            };
-            checkBox1.CheckedChanged += CheckBox1_CheckedChanged;
-
-            listBoxSisteme = new ListBox
-            {
-                Location = new Point(10, 250),
-                Size = new Size(200, 100)
-            };
-
-            comboBoxSisteme = new ComboBox
-            {
-                Location = new Point(220, 250),
-                Size = new Size(200, 21),
-                DropDownStyle = ComboBoxStyle.DropDownList
-            };
-
-            txtIdCautat = new TextBox
-            {
-                Location = new Point(430, 250),
-                Width = 200
-            };
-            txtIdCautat.KeyDown += TxtIdCautat_KeyEnter;
-
-            btnCauta = new Button
-            {
-
-                Text = "Cauta dupa ID",
-                Location = new Point(640, 250),
-                Size = new Size(100, 23)
-            };
-            btnCauta.Click += BtnCauta_Click;
-
-            listBoxSisteme = new ListBox
-            {
-                Location = new Point(300, 100),
-                Size = new Size(200, 100),
-                ScrollAlwaysVisible = true  
-            };
-
-            comboBoxSisteme = new ComboBox
-            {
-                Location = new Point(220, 250),
-                Size = new Size(200, 21),
-                DropDownHeight = 106,  
-                DropDownStyle = ComboBoxStyle.DropDownList,
-                IntegralHeight = false  
-            };
-
-            Controls.AddRange(new Control[] { listBoxSisteme, comboBoxSisteme, txtIdCautat, btnCauta });
-
-            Controls.Add(txtNumeSistem);
-            Controls.Add(txtNumeStea);
-            Controls.Add(txtNrPlanete);
-            Controls.Add(btnAdauga);
-            Controls.Add(lblEroare);
-            Controls.Add(radioButton1);
-            Controls.Add(checkBox1);
-        }
-
-
-        private void InitializeazaListView()
-        {
-            lvSistemeSolare = new ListView
-            {
-                Location = new Point(10, 10),
-                Size = new Size(this.Width - 20, this.Height - 130),
-                View = View.Details,
-                FullRowSelect = true,
-                GridLines = true,
-                Scrollable = true
-            };
-
-            lvSistemeSolare.Columns.Add("ID", -2, HorizontalAlignment.Left);
-            lvSistemeSolare.Columns.Add("Nume Sistem", -2, HorizontalAlignment.Left);
-            lvSistemeSolare.Columns.Add("Soare", -2, HorizontalAlignment.Left);
-            lvSistemeSolare.Columns.Add("Nr. Planete", -2, HorizontalAlignment.Left);
-
-            Controls.Add(lvSistemeSolare);
+            dataGridView1.Columns.Add("ID", "ID");
+            dataGridView1.Columns.Add("NumeSistem", "Nume Sistem");
+            dataGridView1.Columns.Add("Soare", "Soare");
+            dataGridView1.Columns.Add("NrPlanete", "Nr. Planete");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -147,9 +31,104 @@ namespace SistemSolarUI
             IncarcaDateInListe();
         }
 
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            AdaugaSistemSolar();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            BtnCauta_Click();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                labelError.Text = "este bifat.";
+            }
+            else
+            {
+                labelError.Text = "a fost debifat.";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                labelError.Text = " 'Optiune 1' selectata.";
+            }
+        }
+
+        private void AdaugaSistemSolar()
+        {
+            labelError.Text = "";
+
+            if (string.IsNullOrWhiteSpace(textBoxNameSystem.Text) ||
+                string.IsNullOrWhiteSpace(textBoxStarName.Text) ||
+                string.IsNullOrWhiteSpace(textBoxPlanetNumber.Text))
+            {
+                labelError.Text = "Toate campurile trebuie completate.";
+                return;
+            }
+
+            if (!int.TryParse(textBoxPlanetNumber.Text, out int nrPlanete) || nrPlanete < 0)
+            {
+                labelError.Text = "Numarul de planete trebuie sa fie un numar intreg si pozitiv.";
+                return;
+            }
+
+            int idNou = dataGridView1.Rows.Count;
+            dataGridView1.Rows.Add(idNou.ToString(), textBoxNameSystem.Text, textBoxStarName.Text, nrPlanete.ToString());
+            listBoxSystems.Items.Add($"{idNou}: {textBoxNameSystem.Text}, {textBoxStarName.Text}, {nrPlanete}");
+            comboBoxSystems.Items.Add($"{idNou}: {textBoxNameSystem.Text}, {textBoxStarName.Text}, {nrPlanete}");
+
+            SalveazaSistemSolarInFisier(idNou, textBoxNameSystem.Text, textBoxStarName.Text, nrPlanete);
+
+            textBoxNameSystem.Clear();
+            textBoxStarName.Clear();
+            textBoxPlanetNumber.Clear();
+        }
+
+        private void SalveazaSistemSolarInFisier(int id, string numeSistem, string numeStea, int nrPlanete)
+        {
+            using (StreamWriter sw = File.AppendText(CALE_FISIER))
+            {
+                sw.WriteLine($"{id}: {numeSistem}, {numeStea}, {nrPlanete}");
+            }
+        }
+
+        private void BtnCauta_Click()
+        {
+            if (!int.TryParse(textBoxSearchID.Text, out int idCautat))
+            {
+                MessageBox.Show("Introduceti un ID valid.");
+                return;
+            }
+
+            var liniiFisier = File.ReadAllLines(CALE_FISIER);
+            var linieGasita = liniiFisier.FirstOrDefault(linie => linie.StartsWith($"{idCautat}:"));
+
+            if (linieGasita != null)
+            {
+                listBoxSystems.Items.Clear();
+                comboBoxSystems.Items.Clear();
+
+                listBoxSystems.Items.Add(linieGasita);
+                comboBoxSystems.Items.Add(linieGasita);
+            }
+            else
+            {
+                MessageBox.Show("ID-ul introdus nu a fost gasit.");
+                listBoxSystems.Items.Clear();
+                comboBoxSystems.Items.Clear();
+            }
+        }
+
         private void AfiseazaSistemeSolare()
         {
-            lvSistemeSolare.Items.Clear();
+            dataGridView1.Rows.Clear();
 
             if (!File.Exists(CALE_FISIER))
             {
@@ -177,126 +156,88 @@ namespace SistemSolarUI
 
                 if (dateSistem.Length == 3)
                 {
-                    ListViewItem item = new ListViewItem(new[] { id, dateSistem[0], dateSistem[1], dateSistem[2] });
-                    lvSistemeSolare.Items.Add(item);
+                    dataGridView1.Rows.Add(id, dateSistem[0], dateSistem[1], dateSistem[2]);
                 }
-            }
-        }
-
-        private void btnAdauga_Click(object sender, EventArgs e)
-        {
-            AdaugaSistemSolar();
-        }
-
-        private void AdaugaSistemSolar()
-        {
-            lblEroare.Text = "";
-
-            if (string.IsNullOrWhiteSpace(txtNumeSistem.Text) ||
-                string.IsNullOrWhiteSpace(txtNumeStea.Text) ||
-                string.IsNullOrWhiteSpace(txtNrPlanete.Text))
-            {
-                lblEroare.Text = "Toate campurile trebuie completate.";
-                return;
-            }
-
-            if (!int.TryParse(txtNrPlanete.Text, out int nrPlanete) || nrPlanete < 0)
-            {
-                lblEroare.Text = "Numarul de planete trebuie sa fie un numar intreg si pozitiv.";
-                return;
-            }
-
-            int idNou = lvSistemeSolare.Items.Count;
-            ListViewItem item = new ListViewItem(new[] { idNou.ToString(), txtNumeSistem.Text, txtNumeStea.Text, nrPlanete.ToString() });
-            lvSistemeSolare.Items.Add(item);
-            listBoxSisteme.Items.Add($"{idNou}: {txtNumeSistem.Text}, {txtNumeStea.Text}, {nrPlanete}");
-            comboBoxSisteme.Items.Add($"{idNou}: {txtNumeSistem.Text}, {txtNumeStea.Text}, {nrPlanete}");
-
-            SalveazaSistemSolarInFisier(idNou, txtNumeSistem.Text, txtNumeStea.Text, nrPlanete);
-
-            txtNumeSistem.Clear();
-            txtNumeStea.Clear();
-            txtNrPlanete.Clear();
-        }
-
-
-        private void SalveazaSistemSolarInFisier(int id, string numeSistem, string numeStea, int nrPlanete)
-        {
-            using (StreamWriter sw = File.AppendText(CALE_FISIER))
-            {
-                sw.WriteLine($"{id}: {numeSistem}, {numeStea}, {nrPlanete}");
-            }
-        }
-
-        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (((RadioButton)sender).Checked)
-            {
-                lblEroare.Text = " 'Optiune 1' selectata.";
-            }
-        }
-
-        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (((CheckBox)sender).Checked)
-            {
-                lblEroare.Text = "este bifat.";
-            }
-            else
-            {
-                lblEroare.Text = "a fost debifat.";
-            }
-        }
-
-        private void BtnCauta_Click(object sender, EventArgs e)
-        {
-            if (!int.TryParse(txtIdCautat.Text, out int idCautat))
-            {
-                MessageBox.Show("Introduceti un ID valid.");
-                return;
-            }
-
-            var liniiFisier = File.ReadAllLines(CALE_FISIER);
-            var linieGasita = liniiFisier.FirstOrDefault(linie => linie.StartsWith($"{idCautat}:"));
-
-            if (linieGasita != null)
-            {
-                listBoxSisteme.Items.Clear();
-                comboBoxSisteme.Items.Clear();
-
-                listBoxSisteme.Items.Add(linieGasita);
-                comboBoxSisteme.Items.Add(linieGasita);
-            }
-            else
-            {
-                MessageBox.Show("ID-ul introdus nu a fost gasit.");
-                listBoxSisteme.Items.Clear();
-                comboBoxSisteme.Items.Clear();
             }
         }
 
         private void IncarcaDateInListe()
         {
             var liniiFisier = File.ReadAllLines(CALE_FISIER);
-            listBoxSisteme.Items.Clear();  
-            comboBoxSisteme.Items.Clear(); 
+            listBoxSystems.Items.Clear();
+            comboBoxSystems.Items.Clear();
 
             foreach (var linie in liniiFisier)
             {
-                listBoxSisteme.Items.Add(linie);
-                comboBoxSisteme.Items.Add(linie);
-            }   
-        }
-
-        private void TxtIdCautat_KeyEnter(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                BtnCauta_Click(this, EventArgs.Empty); 
-                e.SuppressKeyPress = true; 
+                listBoxSystems.Items.Add(linie);
+                comboBoxSystems.Items.Add(linie);
             }
         }
 
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            ModifySistemSolar();
+        }
+
+        private void ModifySistemSolar()
+        {
+            labelError.Text = "";
+
+            if (!int.TryParse(textBoxSearchID.Text, out int idCautat))
+            {
+                MessageBox.Show("Introduceti un ID valid.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBoxNameSystem.Text) ||
+                string.IsNullOrWhiteSpace(textBoxStarName.Text) ||
+                string.IsNullOrWhiteSpace(textBoxPlanetNumber.Text))
+            {
+                labelError.Text = "Toate campurile trebuie completate.";
+                return;
+            }
+
+            if (!int.TryParse(textBoxPlanetNumber.Text, out int nrPlanete) || nrPlanete < 0)
+            {
+                labelError.Text = "Numarul de planete trebuie sa fie un numar intreg si pozitiv.";
+                return;
+            }
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value.ToString() == idCautat.ToString())
+                {
+                    row.Cells[1].Value = textBoxNameSystem.Text;
+                    row.Cells[2].Value = textBoxStarName.Text;
+                    row.Cells[3].Value = nrPlanete.ToString();
+
+                    UpdateFile();
+                    MessageBox.Show("Sistemul Solar a fost modificat.");
+                    return;
+                }
+            }
+
+            MessageBox.Show("ID-ul introdus nu a fost gasit.");
+        }
+
+        private void UpdateFile()
+        {
+            using (StreamWriter sw = new StreamWriter(CALE_FISIER))
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        sw.WriteLine($"{row.Cells[0].Value}: {row.Cells[1].Value}, {row.Cells[2].Value}, {row.Cells[3].Value}");
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
 
     }
 }
